f42dedabe86344fb322398526ee5c6d9f1cdc27e
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2022-12-08 20:11:04 +0100

    add solution 08 complete

--- aoc-08.py
+++ aoc-08.py
@@ -13,7 +13,6 @@ def part1(puzzle_input):
     result = ((columns - 1) * 2) + ((rows - 1) * 2)
     for row in range(1, rows - 1):
         for col in range(1, columns - 1):
-            visible = False
             current_tree = puzzle_input[row][col]
             # Check left
             left_ok = True
@@ -21,82 +20,61 @@ def part1(puzzle_input):
                 if puzzle_input[row][i] >= current_tree:
                     left_ok = False
                     break
-            if left_ok:
-                visible = True
             # Check right
             right_ok = True
             for i in range(col + 1, columns):
                 if puzzle_input[row][i] >= current_tree:
                     right_ok = False
                     break
-            if right_ok:
-                visible = True
             # Check top
             top_ok = True
             for i in range(0, row):
                 if puzzle_input[i][col] >= current_tree:
                     top_ok = False
                     break
-            if top_ok:
-                visible = True
             # Check bottom
             bottom_ok = True
             for i in range(row + 1, rows):
                 if puzzle_input[i][col] >= current_tree:
                     bottom_ok = False
                     break
-            if bottom_ok:
-                visible = True
-            if visible:
+            if any([left_ok, right_ok, top_ok, bottom_ok]):
                 result += 1
     print(result)
 
 
 def part2(puzzle_input):
     """Solve part 2."""
-    test = ['30373', '25512', '65332', '33549', '35390']
-    puzzle_input = test
     columns = len(puzzle_input[0])
     rows = len(puzzle_input)
-    result = ((columns - 1) * 2) + ((rows - 1) * 2)
+    result = 0
     for row in range(1, rows - 1):
         for col in range(1, columns - 1):
-            visible = False
             current_tree = puzzle_input[row][col]
+            left, right, top, bottom = 0, 0, 0, 0
             # Check left
-            left_ok = True
-            for i in range(0, col):
+            for i in range(col - 1, -1, -1):
+                left += 1
                 if puzzle_input[row][i] >= current_tree:
-                    left_ok = False
                     break
-            if left_ok:
-                visible = True
             # Check right
-            right_ok = True
             for i in range(col + 1, columns):
+                right += 1
                 if puzzle_input[row][i] >= current_tree:
-                    right_ok = False
                     break
-            if right_ok:
-                visible = True
             # Check top
-            top_ok = True
-            for i in range(0, row):
+            for i in range(row - 1, -1, -1):
+                top += 1
                 if puzzle_input[i][col] >= current_tree:
-                    top_ok = False
                     break
-            if top_ok:
-                visible = True
             # Check bottom
-            bottom_ok = True
             for i in range(row + 1, rows):
+                bottom += 1
                 if puzzle_input[i][col] >= current_tree:
-                    bottom_ok = False
                     break
-            if bottom_ok:
-                visible = True
-            if visible:
-                result += 1
+            scenic_score = left * right * top * bottom
+            if scenic_score > result:
+                result = scenic_score
     print(result)
 
 
